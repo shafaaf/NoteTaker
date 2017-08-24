378f5cc9bbf4035e4eabe1bf4e9d8e1aa76e8a5c
Shafaaf Hossain <shafaafhossain@shafaafMacbook.local>
2017-08-23 22:34:15 -0400

    getting all notes working

--- server/server.js
+++ server/server.js
@@ -1,22 +1,30 @@
 var pg = require('pg')
 var format = require('pg-format')
 
-
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
+var express    = require('express');        
+var app        = express();               
 var bodyParser = require('body-parser');
+
+var promise = require('bluebird');
+var options = {
+  // Initialization Options
+  promiseLib: promise
+};
+var pgp = require('pg-promise')(options);
+var connectionString = 'postgres://localhost:5432/notes';
+var db = pgp(connectionString);
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;        // set our port
+var port = process.env.PORT || 8080;        
 
 
 // ROUTES FOR OUR API
 // =============================================================================
-var router = express.Router();              // get an instance of the express Router
+var router = express.Router();          
 
 // middleware to use for all requests
 router.use(function(req, res, next) {
@@ -24,61 +32,45 @@ router.use(function(req, res, next) {
     next(); 
 });
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+// Test route (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
 
-router.route('/note')
-	// get all the notes (accessed at GET http://localhost:8080/api/note/add)
-    .get(function(req, res) {
+// GET all the notes (accessed at GET http://localhost:8080/api/note/add)
+router.get('/note', function(req, res) {
         console.log("GET request at /note");
+
         // console.log("req is: ", req);
-        res.json("GET request at /note");
+		db.any('select * from notestable')
+			.then(function (data) {
+				console.log("data is: ", data);
+				res.status(200)
+				.json({
+					status: 'success',
+					data: data,
+					message: 'Retrieved ALL notes'
+				});
+			})
+			.catch(function (err) {
+				console.log("err is: ", err);
+				return next(err);
+			});
+		// res.json("GET request at /note");
     });
 
-router.route('/note/add')
-	// create a note (accessed at POST http://localhost:8080/api/note/add)
-    .post(function(req, res) {
+// Create a note (accessed at POST http://localhost:8080/api/note/add)
+router.post('/note/add', function(req, res) {
     	console.log("post request at /note/add");
     	// console.log("req is: ", req);
     	res.json("post request at /note/add");
     });
     
 
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
+// Register our routes
+app.use('/api', router); // all of our routes will be prefixed with /api
 
-// START THE SERVER
-app.listen(port);
+// Start the server.
 console.log('Server listening on port ' + port);
-
-// var PGUSER = 'shafaafhossain'
-// var PGDATABASE = 'examplez'
-// var age = 724
-// var config = {
-//   user: PGUSER, // name of the user account
-//   database: PGDATABASE, // name of the database
-//   max: 10, // max number of clients in the pool
-//   idleTimeoutMillis: 30000 // how long a client is allowed to remain idle before being closed
-// }
-
-// var pool = new pg.Pool(config)
-// var myClient
-
-// pool.connect(function (err, client, done) {
-//   if (err) console.log(err)
-//   app.listen(3000, function () {
-//     console.log('listening on 3000')
-//   })
-//   myClient = client
-//   var ageQuery = format('SELECT * from numbers WHERE age = %L', age)
-//   myClient.query(ageQuery, function (err, result) {
-//     if (err) {
-//       console.log(err)
-//     }
-//     console.log(result.rows[0])
-//   })
-// })
+app.listen(port);
 
